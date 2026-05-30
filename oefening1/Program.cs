e7e53e3b323e1095c76ea8c74482e60c7ebf39ef
Thelegendofboydens <ArnaudB803@lln.campussintursula.be>
2026-05-30 10:55:34 +0200

    V1.0.0

--- oefening1/Program.cs
+++ oefening1/Program.cs
@@ -1,1 +1,75 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Test1
+{
+    static void Main(string[] args)
+    {
+        start();
+    }
+    static void start()
+    {
+        Console.WriteLine("Calculator active do you wish to use it? (y/n) ");
+        string? input = Console.ReadLine();
+        if (input == "y")
+        {
+            Calculate();
+        }
+        else
+        {
+            Console.WriteLine("Calculator closed");
+        }
+    }
+    static void cont(){
+        Console.WriteLine("do you wish to continue using the calculator y/n");
+        string? ask = Console.ReadLine();
+        if (ask == "y")
+        {
+            Calculate();
+        }
+        else if (ask == "n")
+        {
+            Console.WriteLine("calculator clodsed");
+        }
+      
+        
+    }
+    static void Calculate()
+    {
+         int a =0;
+        int b = 0;
+        int answer = 0;
+        Console.WriteLine("Enter the first number:");
+        a= int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter the second number:");
+        b= int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the operation (+, -, *, /):");
+        string? operation = Console.ReadLine();
+        if (operation == "+")
+        {
+            answer += a+b;
+            
+            Console.WriteLine("the result is: "+ answer);
+            cont();
+        }
+        else if (operation == "/")
+        {
+            answer =  a/b;
+            Console.WriteLine("the result is: "+ answer);
+            cont();
+        }
+        else if (operation == "-")
+        {
+            answer = a-b;
+            Console.WriteLine("the result is: "+ answer);
+            cont();
+        }
+        else
+        {
+            answer = a*b;
+            Console.WriteLine("the result is: "+ answer);
+            cont();
+        }
+        
+    }
+
+    } 
+
